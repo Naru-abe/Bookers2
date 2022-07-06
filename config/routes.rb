9655b7a04813aca6e7ca2c28e949e5ad276f8f5e
Narumi Abe <smile.narukkory@gmail.com>
2022-07-06 07:04:26 +0000

    [Add]guestlogin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
-  
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   resources :books
   resources :users, only: [:new, :create, :show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
